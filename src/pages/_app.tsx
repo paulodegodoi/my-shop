a3f055f4584a35d4d9a23cba0c65d3a503c5f8b9
Paulo <paulogodoi.sp@gmail.com>
2023-05-08 21:35:11 -0300

    add carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {globalStyles} from "@/styles/global";
+import logoImg from "../assets/logo.svg"
+import {Container, Header} from "@/styles/pages/app";
+import Image from "next/image";
 
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+      <Container>
+        <Header>
+            <Image src={logoImg.src} width={100} height={40} alt=""/>
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+  )
 }
