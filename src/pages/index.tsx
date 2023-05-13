d188f46a5fcd409edc60d4738f4f0ff6598ef72f
Paulo <paulogodoi.sp@gmail.com>
2023-05-13 20:20:05 -0300

    links e prefetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,75 @@
-import {styled} from "@/styles";
-import {HomeContainer, Product} from "@/styles/pages/home";
-import Image from "next/image";
-
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Link from "next/link"
 import { useKeenSlider } from "keen-slider/react"
 
-import Image1 from "../assets/camisetas/1.png"
-import Image2 from "../assets/camisetas/2.png"
-import Image3 from "../assets/camisetas/3.png"
-
 import "keen-slider/keen-slider.min.css"
+import { stripe } from "@/lib/stripe"
+import { GetStaticProps } from "next"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: "$green300",
-  borderRadius: 7,
-  border: 0,
-  padding: '4px 8px',
+import Stripe from "stripe"
 
-  span: {
-    fontWeight: "bold"
-  },
+interface HomeProps {
+	products: {
+		id: string
+		name: string
+		imageUrl: string
+		price: string
+	}[]
+}
 
-  "&:hover": {
-    filter: "brightness(0.8)"
-  },
-})
+export default function Home({ products }: HomeProps) {
+	const [sliderRef] = useKeenSlider({
+		slides: {
+			perView: 3,
+			spacing: 48,
+		},
+	})
 
+	return (
+		<HomeContainer ref={sliderRef} className="keen-slider">
+			{products.map((p) => {
+				return (
+					<Product
+						key={p.id}
+						href={`/product/${p.id}`}
+						className="keen-slider__slide"
+						prefetch={false}
+					>
+						<Image src={p.imageUrl} width={520} height={480} alt="" />
+						<footer>
+							<strong>{p.name}</strong>
+							<span>{p.price}</span>
+						</footer>
+					</Product>
+				)
+			})}
+		</HomeContainer>
+	)
+}
 
-export default function Home() {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48
-    }
-  })
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await stripe.products.list({
+		expand: ["data.default_price"],
+	})
 
-  return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={Image1} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={Image2} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={Image3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={Image3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-    </HomeContainer>
-  )
+	const products = response.data.map((product) => {
+		const price = product.default_price as Stripe.Price
+		return {
+			id: product.id,
+			name: product.name,
+			imageUrl: product.images[0],
+			price: new Intl.NumberFormat("pt-BR", {
+				style: "currency",
+				currency: "BRL",
+			}).format(price.unit_amount! / 100),
+		}
+	})
+
+	return {
+		props: {
+			products,
+		},
+		revalidate: 60 * 60 * 2, // 2 hours
+	}
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,62 +1,64 @@
-import {styled} from "@/styles";
+import { styled } from "@/styles"
+import Link from "next/link"
 
 export const HomeContainer = styled("main", {
-    display: "flex",
-    //gap: "3rem",
-    width: "100%",
-    maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
-    marginLeft: "auto",
-    minHeight: 656
+	display: "flex",
+	//gap: "3rem",
+	width: "100%",
+	maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
+	marginLeft: "auto",
+	minHeight: 656,
 })
 
-export const Product = styled("a", {
-    background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
-    borderRadius: 7,
-    //padding: "0.25rem",
-    position: "relative",
-    overflow: "hidden",
+export const Product = styled(Link, {
+	background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+	borderRadius: 7,
+	//padding: "0.25rem",
+	position: "relative",
+	overflow: "hidden",
 
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
 
-    img: {
-        objectFit: "cover"
-    },
+	img: {
+		objectFit: "cover",
+	},
 
-    footer: {
-        position: "absolute",
-        bottom: "0.25rem",
-        left: "0.25rem",
-        right: "0.25rem",
-        padding: "2rem",
-        borderRadius: 7,
+	footer: {
+		position: "absolute",
+		bottom: "0.25rem",
+		left: "0.25rem",
+		right: "0.25rem",
+		padding: "2rem",
+		borderRadius: 7,
 
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
 
-        backgroundColor: "rgba(0, 0, 0, 0.6)",
+		backgroundColor: "rgba(0, 0, 0, 0.6)",
 
-        transform: "translateY(110%)",
-        opacity: 0,
-        transition: "all 0.2s ease-in-out",
+		transform: "translateY(110%)",
+		opacity: 0,
+		transition: "all 0.2s ease-in-out",
 
-        strong: {
-            fontSizes: "$lg"
-        },
+		strong: {
+			fontSizes: "$lg",
+			color: "$gray100",
+		},
 
-        span: {
-            fontSizes: "$xl",
-            fontWeight: "bold",
-            color: "$green300"
-        }
-    },
+		span: {
+			fontSizes: "$xl",
+			fontWeight: "bold",
+			color: "$green300",
+		},
+	},
 
-    "&:hover": {
-        footer: {
-            transform: "translateY(0%)",
-            opacity: 1
-        }
-    }
+	"&:hover": {
+		footer: {
+			transform: "translateY(0%)",
+			opacity: 1,
+		},
+	},
 })
